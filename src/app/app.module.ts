b8e605294b61285bee13d6a4734ffff43ed59c76
Haseeb <haseeb@Haseebs-iMac.local>
2021-12-30 19:07:35 +0500

    working at side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
